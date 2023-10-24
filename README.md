7686364bf103861032220fee807689618e0dddec
jiruneko <oohikaru310@gmail.com>
2023-10-24 16:02:38 +0900

    first commit

--- README.md
+++ README.md
@@ -36,3 +36,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 # kari
 # kari
 # kari
+# portfolio_alpha1
--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_fwoN35iV_LRp9VyxeYRKyuq4FZsjh8vJB);
 const fromEmail = process.env.re_fwoN35iV_LRp9VyxeYRKyuq4FZsjh8vJB;
 
 export async function POST(req, res) {
